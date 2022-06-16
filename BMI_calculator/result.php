00e8fde247913e4f3ab715c854e000c41df133e5
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-06-16 20:04:25 +0800

    add BMI.php

--- BMI_calculator/result.php
+++ BMI_calculator/result.php
@@ -7,6 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        *{
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+
+        }
     td{
         border: 1px solid black;
         text-align: center;
